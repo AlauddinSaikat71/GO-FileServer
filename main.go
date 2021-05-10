12de4d1f2ea486123604ee07230ee2de26bcb262
AlauddinSaikat71 <alauddin27@student.sust.edu>
2021-05-10 18:09:00 +0600

    return '.pdf' , '.html' file

--- main.go
+++ main.go
@@ -4,20 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 )
+
+//temporary directory
+var TmpDir = filepath.FromSlash("G:/Repositories/GoFileserverStorage")
 
 func main() {
 	// create file server handler
-	fs := http.FileServer(http.Dir("G:/Repositories/GoFileserverStorage"))
+	fs := http.FileServer(http.Dir(TmpDir))
 
 	//handle '/' route
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		res.Header().Set("Content-Type", "text/html")
-		fmt.Fprint(res, "<h1>Go Lang</h1>")
+	http.HandleFunc("", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "Hello Golang!")
 	})
 
-	// handle `/static` route
-	http.Handle("/static", fs)
+	//return a '.pdf' file for '/pdf' route
+	http.HandleFunc("/pdf", func(res http.ResponseWriter, req *http.Request) {
+		http.ServeFile(res, req, filepath.Join(TmpDir, "/files/test.pdf"))
+	})
+
+	//return a '.html' file for '/index.html' route
+	http.HandleFunc("/index.html", func(req http.ResponseWriter, res *http.Request) {
+		http.ServeFile(req, res, filepath.Join(TmpDir, "/index.html"))
+	})
 
 	// start HTTP server with `fs` as the default handler
 	log.Fatal(http.ListenAndServe(":9000", fs))
