746b175f34a0185e82396fc3393986b9d08ef31b
AlauddinSaikat71 <alauddin27@student.sust.edu>
2021-05-10 19:52:43 +0600

    saveFileHandler

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gofileserver
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.1 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.1 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,30 +5,45 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
+
+	"github.com/gin-gonic/gin"
 )
 
-//temporary directory
-var TmpDir = filepath.FromSlash("G:/Repositories/GoFileserverStorage")
+func saveFileHandler(c *gin.Context) {
+    file, err := c.FormFile("file")
+
+    // The file cannot be received.
+    if err != nil {
+        c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+            "message": "No file is received",
+        })
+        return
+    }
+
+    // Retrieve file information
+    extension := filepath.Ext(file.Filename)
+    // Generate random file name for the new uploaded file so it doesn't override the old file with same name
+    newFileName := uuid.New().String() + extension
+
+    // The file is received, so let's save it
+    if err := c.SaveUploadedFile(file, "/some/path/on/server/" + newFileName); err != nil {
+        c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+            "message": "Unable to save the file",
+        })
+        return
+    }
+
+    // File saved successfully. Return proper result
+    c.JSON(http.StatusOK, gin.H{
+        "message": "Your file has been successfully uploaded."
+    })
+}
 
 func main() {
-	// create file server handler
-	fs := http.FileServer(http.Dir(TmpDir))
-
-	//handle '/' route
-	http.HandleFunc("", func(res http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(res, "Hello Golang!")
-	})
-
-	//return a '.pdf' file for '/pdf' route
-	http.HandleFunc("/pdf", func(res http.ResponseWriter, req *http.Request) {
-		http.ServeFile(res, req, filepath.Join(TmpDir, "/files/test.pdf"))
-	})
-
-	//return a '.html' file for '/index.html' route
-	http.HandleFunc("/index.html", func(req http.ResponseWriter, res *http.Request) {
-		http.ServeFile(req, res, filepath.Join(TmpDir, "/index.html"))
-	})
-
-	// start HTTP server with `fs` as the default handler
-	log.Fatal(http.ListenAndServe(":9000", fs))
+	r := gin.Default
+	r.POST("/uploads/image",saveFileHandler())
+	
+	//http.ListenAndServe(":9000",r)
+	
+	r.Run()
 }
