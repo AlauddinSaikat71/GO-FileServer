2e10623b5d3937f6bfb9571332b1144c4a5d28a3
AlauddinSaikat71 <alauddin27@student.sust.edu>
2021-05-10 14:21:26 +0600

    file root access

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
-import(
 
+import (
+	"fmt"
+	"log"
+	"net/http"
 )
-func main(){
-	
+
+func main() {
+	// create file server handler
+	fs := http.FileServer(http.Dir("G:/Repositories/GoFileserverStorage"))
+
+	//handle '/' route
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(res, "<h1>Go Lang</h1>")
+	})
+
+	// handle `/static` route
+	http.Handle("/static", fs)
+
+	// start HTTP server with `fs` as the default handler
+	log.Fatal(http.ListenAndServe(":9000", fs))
 }
